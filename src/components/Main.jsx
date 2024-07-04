7aa2a621a3267008f067d6d69fea75056ae231a1
torihazi <lovebarusa19981225@icloud.com>
2024-07-04 10:36:28 +0900

    fin chapter 59

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./Main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder="ノート内容を記入"></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">
+          <div className="markdown-preview">ノート内容</div>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
